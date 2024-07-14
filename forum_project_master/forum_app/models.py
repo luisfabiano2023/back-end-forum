63fe6d610592815fb182884dc3dcecc69c3874cf
luisfabiano2023 <luisfabiano2022.1@gmail.com>
2024-07-14 01:37:40 -0300

    criando models

--- forum_project_master/forum_app/models.py
+++ forum_project_master/forum_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+import date,datetime as dt
 
 # Create your models here.
+
+class users:
+    username = models.CharField()
+    email= models.EmailField()
+    psswd=models.CharField()
+class post_structure:
+    title=models.CharField(max_length=150)
+    content=models.CharField(max_length=600)
+    post_category=self.category.id
+    hour_post = dt.datetime.now()
+    
+    def __str__(self) -> str:
+        pass
+
+
+class category:
+    ct_name=models.CharField(max_length=80)
+    id=models.UUIDField()
+
+
