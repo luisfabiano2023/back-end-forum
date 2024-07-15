5fee2d8d18e34c7efce03166082946f36d6baee0
luisfabiano2023 <luisfabiano2022.1@gmail.com>
2024-07-15 07:15:55 -0300

    criando os serializadores

--- forum_project_master/forum_app/serializers.py
+++ forum_project_master/forum_app/serializers.py
@@ -1,9 +1,25 @@
 from django.db import models
 from . import models
 from rest_framework import serializers
-
-
 class userS(serializers.ModelSerializer):
     class meta:
-        model = user
-        fields  = ['username','email','psswd']
+        model = models.users
+        fields  = ['id',
+                   'username',
+                   'email',
+                   'psswd'
+                   ]
+class postS(serializers.ModelSerializer):
+    class meta:
+        model=models.post_structure
+        fields=['id',
+                  'title',
+                  'content',
+                  'post_category',
+                  'hour_post'
+                  ]
+class category(serializers.ModelSerializer):
+    class meta:
+        model=models.category
+        fields=['id',
+                'ct_name']
