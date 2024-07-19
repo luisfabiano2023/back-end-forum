d218bafcac108cf2440407a638646d3d821122ab
Luis Barbosa <130789397+luisfabiano2023@users.noreply.github.com>
2024-07-19 18:17:24 +0000

    Update models.py

--- forum_project_master/forum_app/models.py
+++ forum_project_master/forum_app/models.py
@@ -1,23 +1,21 @@
 from django.db import models
-import date,datetime as dt
+import datetime as dt
 
 # Create your models here.
 
-class users:
-    username = models.CharField()
+
+class category(models.Model):
+    ct_name=models.CharField(max_length=80)
+    id=models.UUIDField().primary_key=True
+
+class users(models.Model):
+    username = models.CharField(max_length=20)
     email= models.EmailField()
-    psswd=models.CharField()
-class post_structure:
+    psswd=models.CharField(max_length=12)
+
+class post_structure(models.Model):
     title=models.CharField(max_length=150)
     content=models.CharField(max_length=600)
-    post_category=self.category.id
+    post_category=category.id
     hour_post = dt.datetime.now()
-    
-    def __str__(self) -> str:
-        pass
-
-
-class category:
-    ct_name=models.CharField(max_length=80)
-    id=models.UUIDField()
-
+    author=users.username
