11ad3f1efc346e4f52045e218b463c0dd3641419
luisfabiano2023 <luisfabiano2022.1@gmail.com>
2024-07-19 16:41:27 -0300

    criando view postar

--- forum_project_master/forum_app/views.py
+++ forum_project_master/forum_app/views.py
@@ -3,17 +3,26 @@ from . import models as md
 from . import serializers as se
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-def postar():
+from rest_framework import status
+@api_view(['POST'])
+def postar(request): #criando um novo post 
+    post=se.postS(data=request.data)
+    if md.post.objects.filter(**request.data).exists():
+        raise se.ValidationError('This data already exists')
+ 
+    if post.is_valid():
+        post.save()
+        return Response(post.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
     
-    pass
 def cadastrar_se():
     pass
 def logar():
     pass
+
 def mostrar_post():
     pass
+
 def apaga_user():
-    pass
-def create_user():
-    
     pass
